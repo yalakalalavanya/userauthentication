36c25655f4a3568973ba113ae30703531e1b9626
yalakalalavanya <lavanya82645@gmail.com>
2024-09-22 16:53:45 +0530

    first commit

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ users = {}
 
 @app.route('/')
 def index():
-    return render_template('login.html')
+    return render_template('index.html')
 
 @app.route('/register', methods=['POST'])
 def register():
